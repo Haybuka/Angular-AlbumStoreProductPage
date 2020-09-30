a14ed77f00e1e8967eea38b1d1736ac83a5b2506
Haybuka <paschal.nduleo@gmail.com>
2020-09-30 01:34:02 +0100

    added new imports

---
+++ src/app/product.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import { Response } from "@angular/http";
+@Injectable()
+export class ProductService {
+  constructor(private _http: Http) {}
+}
