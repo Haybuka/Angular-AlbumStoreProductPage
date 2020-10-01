111ded5e861af4b3c82f3d1fff5a29e4a5e6e6f0
Haybuka <paschal.nduleo@gmail.com>
2020-10-01 01:10:24 +0100

    readying up the router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from "./app.component";
 import { ProductPageComponent } from "./product-page/product-page.component";
 import { ProductDescriptionComponent } from "./product-description/product-description.component";
 import { ProductService } from "./product.service";
-import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
+import { ProductTracklistingComponent } from "./product-tracklisting/product-tracklisting.component";
+import { ProductListComponent } from "./product-list/product-list.component";
+import { ProductListingComponent } from "./product-listing/product-listing.component";
 @NgModule({
   declarations: [
     AppComponent,
     ProductPageComponent,
     ProductDescriptionComponent,
     ProductTracklistingComponent,
+    ProductListComponent,
+    ProductListingComponent,
   ],
   imports: [BrowserModule, FormsModule, HttpModule],
   providers: [ProductService],
